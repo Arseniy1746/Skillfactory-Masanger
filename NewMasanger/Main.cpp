be091cbaed6d556603bc4794ea61dc6e1fc84703
RyoYamada <lewaden6@gmail.com>
2026-02-17 19:34:48 +0300

    Add user menu

--- NewMasanger/Main.cpp
+++ NewMasanger/Main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 #include <memory>
 using namespace std;
+
+// Меню
+void Register_Menu() {
+    cout << endl << "        ========================== " << endl;
+    cout << "        |        Ч А Т           |" << endl;
+    cout << "        |                        |" << endl;
+    cout << "        | 1. Регистрация         |" << endl;
+    cout << "        | 2. Вход                |" << endl;
+    cout << "        | 3. Выход из программы  |" << endl;
+    cout << "        ==========================" << endl;
+    cout << "Выберите действие: ";
+}
+
+void User_Menu() {
+    cout << endl << "        ==========================" << endl;
+    cout << "        |   Добро пожаловать!    |" << endl;
+    cout << "        | 1. Написать всем       |" << endl;
+    cout << "        | 2. Личное сообщение    |" << endl;
+    cout << "        | 3. Показать общий чат  |" << endl;
+    cout << "        | 4. Показать мои ЛС     |" << endl;
+    cout << "        | 5. Выйти из аккаунта   |" << endl;
+    cout << "        | 6. Список пользователей|" << endl;
+    cout << "        ==========================" << endl;
+    cout << "Выберите действие: ";
+}
+
+
+
+
+
 int main()
 {
 
